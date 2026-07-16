639cacf1b0011b0d5882d9a228ce8da3cc2fdb14
Martha David Londoño <davidosiris88@gmail.com>
2026-07-15 20:25:53 -0500

    ok

---
+++ pulsaciones.py
@@ -0,0 +1,20 @@
+#Calcule el número de pulsaciones que debe tener una persona cada 10 segundos de ejercicio aeróbico,
+#según su sexo y edad. Solicite la edad (en años) y el sexo (F = femenino, M = masculino), y aplique:
+#• Sexo femenino: num_pulsaciones = (220 − edad) / 10
+#• Sexo masculino: num_pulsaciones = (210 − edad) / 10
+#Imprima el número de pulsaciones recomendadas.
+
+edad = int(input("Ingrese su edad (en años): "))
+sexo = input("Ingrese su sexo (F = femenino, M = masculino): ").upper()
+pulsa=10
+fem=220
+masc=210
+
+if sexo == "F":
+    num_pulsaciones = (fem - edad) / pulsa
+elif sexo == "M":
+    num_pulsaciones = (masc - edad) / pulsa
+else:
+    print("Sexo no válido. Por favor, ingrese 'F' para femenino o 'M' para masculino.")
+
+print(f"El número de pulsaciones recomendadas es: {num_pulsaciones}")
